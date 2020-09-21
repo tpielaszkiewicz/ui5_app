7f2b68e891c7cdac5f83521ae5ddd645713a5ae3
tpielaszkiewicz <30354909+tpielaszkiewicz@users.noreply.github.com>
2020-09-21 21:39:34 +0200

    commit

--- startscript.js
+++ startscript.js
@@ -1,6 +1,7 @@
 const { exec } = require("child_process");
 
 var port = process.env.PORT || 3219;
+console.log(exec);
 console.log("Use port:" + port);
 exec(`ui5 serve -p ${port}`, (error, stdout, stderr) => {
     if (error) {
