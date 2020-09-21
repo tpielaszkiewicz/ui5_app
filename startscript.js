6d0160e13edcdc3fbc32681be399948ed342cea5
tpielaszkiewicz <30354909+tpielaszkiewicz@users.noreply.github.com>
2020-09-21 15:26:05 +0200

    port fixed

--- startscript.js
+++ startscript.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 
 var port = process.env.PORT || 3219;
-exec(`ui5 serve -o -p ${port}`, (error, stdout, stderr) => {
+exec(`ui5 serve -p ${port}`, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
