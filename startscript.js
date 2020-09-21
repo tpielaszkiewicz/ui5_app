673eeb11709710897423ed6c11e2922f20b5a8ba
tpielaszkiewicz <30354909+tpielaszkiewicz@users.noreply.github.com>
2020-09-21 21:41:55 +0200

    some push

--- startscript.js
+++ startscript.js
@@ -3,7 +3,7 @@ const { exec } = require("child_process");
 var port = process.env.PORT || 3219;
 console.log(exec);
 console.log("Use port:" + port);
-exec(`ui5 serve -p ${port}`, (error, stdout, stderr) => {
+exec(`ui5 serve -p ${port} -o`, (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
